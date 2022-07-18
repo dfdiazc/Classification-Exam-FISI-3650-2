23b8a0269f5f2a5384a4297093dc7424705a57e7
dfdiazc <df.diazc@uniandes.edu.co>
2022-07-18 13:34:32 -0500

    Added Changes

--- README.md
+++ README.md
@@ -8,10 +8,10 @@ The project aims to obtain a model, a convolutional neural network in this case,
 
 ## The model
 
-Initially, the dataset is loaded, using 70% of the images for training, while the remaining 30% will be used for validation. The images are loaded into the model in batches of 16, and were resized to 256x256 pixels in order to optimize CPU and GPU usage. Then the model itself is defined. The first layer rescales the images and normalizes the RGB values. Then, three convolution layers are defined, each with a corresponding pooling layer and ReLU activation functions. A dropout layer is also defined in order to improve the quality of the model. Finally, two dense, fully connected layers are defined. The first one is a 512 unit layer with a ReLU activation function, same as before. The second one is the final output layer, a 2 unit one with a Softmax activation function.
+Initially, the dataset is loaded, using 90% of the images for training, while the remaining 10% will be used for validation. The images are loaded into the model in batches of 16, and were resized to 80x80 pixels in order to optimize CPU and GPU usage. Then the model itself is defined. The first layer rescales the images and normalizes the RGB values. Then, three convolution layers are defined, each with a corresponding pooling layer and ReLU activation functions. A dropout layer is also defined in order to improve the quality of the model. Finally, two dense, fully connected layers are defined. The first one is a 512 unit layer with a ReLU activation function, same as before. The second one is the final output layer, a 2 unit one with a Softmax activation function.
 
 Having defined the model, the Adam optimizer is chosen and a Cross Entropy function is defined for the loss function. Now, the model is trained for 10 epochs and saved in a .h5 file.
 
 ## Results
 
-Using the obtained model it is possile now to run the *eval.py* file, obtaining a 88.78% accuracy for the **Test** data.
+Using the obtained model it is possile now to run the *eval.py* file, obtaining a 95.05% accuracy for the **Test** data.
--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class Model():
         self.model = tf.keras.models.load_model('model.h5')
         
     def predict(self, file_path):
-        img = tf.keras.preprocessing.image.load_img(file_path, target_size=(50, 50))
+        img = tf.keras.preprocessing.image.load_img(file_path, target_size=(80, 80))
         img_array = tf.keras.preprocessing.image.img_to_array(img)
         img_array = tf.expand_dims(img_array, 0)
         
@@ -18,25 +18,25 @@ class Model():
         return prediction
         
     def train_model(self):
-        data_dir = 'Rice_Image_Dataset/Train'
-        batch_size = 64
-        img_height = 50
-        img_width = 50
+        data_dir = 'chest_xray_dataset/train'
+        batch_size = 32
+        img_height = 80
+        img_width = 80
         
         train_ds = tf.keras.utils.image_dataset_from_directory(
             data_dir,
-            validation_split = 0.2,
+            validation_split = 0.1,
             subset = 'training',
-            seed = 42,
+            seed = 123,
             image_size = (img_height, img_width),
             batch_size = batch_size
         )
         
         val_ds = tf.keras.utils.image_dataset_from_directory(
             data_dir,
-            validation_split = 0.2,
+            validation_split = 0.1,
             subset = 'validation',
-            seed = 42,
+            seed = 123,
             image_size = (img_height, img_width),
             batch_size = batch_size
         )
@@ -49,17 +49,29 @@ class Model():
         train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
         val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
         
+        data_augmentation = tf.keras.Sequential(
+        [
+            tf.keras.layers.RandomFlip("horizontal",
+                            input_shape=(img_height,
+                                        img_width,
+                                        3)),
+            tf.keras.layers.RandomRotation(0.1),
+            tf.keras.layers.RandomZoom(0.1),
+        ]
+        )
+        
         model = tf.keras.Sequential([
+            data_augmentation,
             tf.keras.layers.Rescaling(1./255),
+            tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+            tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
             tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
             tf.keras.layers.MaxPooling2D(),
-            tf.keras.layers.Conv2D(128, 3, padding='same', activation='relu'),
-            tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dense(512, activation='relu'),
             tf.keras.layers.Dense(num_classes, activation='softmax')
         ])
         
